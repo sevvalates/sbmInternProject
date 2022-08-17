822cbf55f87e832a9cb4d7660f18f623fc5defe4
sevvalates <57041105+sevvalates@users.noreply.github.com>
2022-08-17 10:32:45 +0300

    solve database errors

--- InsuranceAgency/src/main/java/com/sbmInternProject/InsuranceAgency/Controllers/UserController.java
+++ InsuranceAgency/src/main/java/com/sbmInternProject/InsuranceAgency/Controllers/UserController.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@Controller // This means that this class is a Controller
+@RestController // This means that this class is a Controller
 @RequestMapping(path="/") // This means URL's start with / (after Application path)
 public class UserController {
 
