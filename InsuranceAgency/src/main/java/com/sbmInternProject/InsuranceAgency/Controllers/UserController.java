ed8195da9a0af842e31be788f19fa88e933f3605
sevvalates <57041105+sevvalates@users.noreply.github.com>
2022-09-12 14:48:42 +0300

    update

--- InsuranceAgency/src/main/java/com/sbmInternProject/InsuranceAgency/Controllers/UserController.java
+++ InsuranceAgency/src/main/java/com/sbmInternProject/InsuranceAgency/Controllers/UserController.java
@@ -5,12 +5,8 @@ import com.sbmInternProject.InsuranceAgency.Services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
-
 import javax.validation.Valid;
-
 
 @Controller
 public class UserController {
@@ -19,24 +15,6 @@ public class UserController {
     public UserController(UserService userService) {
         this.userService = userService;
     }
-    /*
-    @RequestMapping(value= "/register", method = RequestMethod.GET)
-    public ModelAndView getRegisterPage() {
-        return new ModelAndView("register", "user", new User());
-    }
-
-    @RequestMapping(value = "/register", method = RequestMethod.POST)
-    public String handleRegisterForm(@Valid @ModelAttribute("user") User user, BindingResult bindingResult) {
-        if (bindingResult.hasErrors())
-            return "register";
-        userService.addUser(user);
-        return "redirect:/";
-    }
-
-    @RequestMapping("/users")
-    public ModelAndView getUsersPage() {
-        return new ModelAndView("users", "users", userService.getUsers());
-    }*/
 
     //@GetMapping("/register")
     @RequestMapping(value = "/register", method = RequestMethod.GET)
